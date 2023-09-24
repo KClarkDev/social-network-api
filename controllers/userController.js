efa836e70fe89bdd77bb5e533bd29d3392f249fb
Katherine Clark <60265279+KClarkDev@users.noreply.github.com>
2023-09-24 14:51:48 -0400

    Added seeding script

---
+++ controllers/userController.js
@@ -0,0 +1,77 @@
+const { ObjectId } = require("mongoose").Types;
+const { User } = require("../models");
+
+module.exports = {
+  // Get all users
+  async getUsers(req, res) {
+    try {
+      const users = await User.find();
+
+      const userObj = {
+        users,
+      };
+
+      res.json(userObj);
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json(err);
+    }
+  },
+  // Get a single user
+  async getSingleUser(req, res) {
+    try {
+      const user = await User.findOne({
+        _id: req.params.userId,
+      }).select("-__v");
+
+      if (!user) {
+        return res.status(404).json({ message: "No user with that ID" });
+      }
+
+      res.json({
+        user,
+      });
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json(err);
+    }
+  },
+  // create a new user
+  async createUser(req, res) {
+    try {
+      const user = await User.create(req.body);
+      res.json(user);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+  // Delete a user and remove their associated thoughts
+  async deleteUser(req, res) {
+    try {
+      const user = await User.findOneAndRemove({
+        _id: req.params.userId,
+      });
+
+      if (!user) {
+        return res.status(404).json({ message: "No such user exists" });
+      }
+
+      const thoughts = await Thought.findAllAndUpdate(
+        { users: req.params.userId },
+        { $pull: { users: req.params.userId } },
+        { new: true }
+      );
+
+      if (!thoughts) {
+        return res.status(404).json({
+          message: "Student deleted, but no thoughts found",
+        });
+      }
+
+      res.json({ message: "Student successfully deleted" });
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  },
+};
---
+++ utils/seed.js
@@ -0,0 +1,38 @@
+const connection = require("../config/connection");
+const { User } = require("../models");
+const { getRandomName } = require("./data");
+
+connection.on("error", (err) => err);
+
+connection.once("open", async () => {
+  console.log("connected");
+
+  let usersCheck = await connection.db
+    .listCollections({ name: "users" })
+    .toArray();
+  if (usersCheck.length) {
+    await connection.dropCollection("users");
+  }
+
+  // Create empty array to hold the users
+  const users = [];
+
+  // Loop 20 times -- add users to the user array
+  for (let i = 0; i < 20; i++) {
+    const username = getRandomName();
+    const email = username.split(" ")[0] + "@gmail.com";
+
+    users.push({
+      username,
+      email,
+    });
+  }
+
+  // Add students to the collection and await the results
+  await User.collection.insertMany(users);
+
+  // Log out the seed data to indicate what should appear in the database
+  console.table(users);
+  console.info("Seeding complete! 🌱");
+  process.exit(0);
+});
